b79fe67e1d6a901d0dce82a840cc257a71c6e9a1
Evgeniy <lnf_55@mail.ru>
2023-03-28 11:53:21 +0300

    добавил задачи 26.28.41.45

--- task28.py
+++ task28.py
@@ -3,3 +3,13 @@
 # *Пример:*
 # 2 2
 #     4
+def sum (num1, num2):
+    num1 += 1
+    num2 -= 1
+    if num2 > 0: return sum(num1, num2)
+    else: return num1
+ 
+number_a = int(input('Введите число A: '))
+number_b = int(input('Введите число B: '))
+
+print (sum(number_a, number_b))
