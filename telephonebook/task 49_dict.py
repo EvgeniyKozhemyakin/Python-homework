a4a2c3455fd85aeec498830cfe29ad65e9b137fc
Evgeniy <lnf_55@mail.ru>
2023-04-09 17:51:51 +0300

    добавил задание с телефонным справочником

--- telephonebook/task 49_dict.py
+++ telephonebook/task 49_dict.py
@@ -6,39 +6,96 @@
 # текстовом файле
 # 3. Пользователь может ввести одну из характеристик для поиска определенной записи(Например имя или фамилию человека)
 # 4. Использование функций. Ваша программа не должна быть линейной
+import os
 
 
 def read_file():
-    with open(path_file,'r',encoding='UTF-8') as f:
-       for line in f:
-          print(line.strip())
+    with open(path_file, 'r', encoding='UTF-8') as f:
+        print('Телефонная книга пользователя')
+        for line in f:
+            print(line.strip())
+
 
 def write_file():
-    with open(path_file,'a',encoding='UTF-8' ) as f:
-        f.writelines('\n' + input())
+    with open(path_file, 'a', encoding='UTF-8') as f:
+        f.writelines('\n' + input('Введите данные контакта: '))
+
 
 def find_info():
     find_info = input('Что хотите найти: ')
-    with open(path_file,'r',encoding='UTF-8' ) as f:       
+    with open(path_file, 'r', encoding='UTF-8') as f:
         for line in f:
             if find_info.casefold() in line.casefold():
                 print(line)
 
-def change_file():
-    find_info = input()
-    new_info = input()
-    with open(path_file,'r+',encoding='UTF-8') as f:
+
+def find_contact(find_info):
+    contact = ''
+    contact_none = 'Такого контакта нет в телефонной книге.'
+    with open(path_file, 'r', encoding='UTF-8') as f:
         for line in f:
             if find_info.casefold() in line.casefold():
-                if input("Да/Нет") == "Да":
-                    lst = (line.strip()).split(' ')
-                    print(lst)
-                else: continue
+                contact = line                              
+    if contact == '':print(contact_none)
+    return contact
 
-        
+
+def change_file():
+    name = find_contact(
+        input('Введите данные контакта,которые необходимо поменять: '))
+    with open("telephonebook/temp.txt", "w", encoding="UTF-8") as temp, open(path_file, 'r', encoding='UTF-8') as f:
+        contact = ''
+        for line in f:
+            if line != name:
+                temp.writelines(line)
+            elif input(f'{name} Вы хотите изменить контакт ? (да/нет): ') == 'да'.casefold():
+                    contact = input('Введите новые данные контакта: ')
+                    temp.writelines(contact + '\n')
+                    print('Контакт изменен!')
+            else: 
+                temp.writelines(name)    
+    os.remove('telephonebook/telephonebook.txt')
+    os.rename("telephonebook/temp.txt", "telephonebook/telephonebook.txt")
+
+
+def delete_contact():
+    name = find_contact(
+        input('Введите данные контакта,которые необходимо удалить: '))
+    with open("telephonebook/temp.txt", "w", encoding="UTF-8") as temp, open(path_file, 'r', encoding='UTF-8') as f:
+
+        for line in f:
+            if line != name:
+                temp.writelines(line)
+            elif input(f'Вы хотите удалить контакт: \n{name}(да/нет): ') == 'да'.casefold():
+                print('Контакт изменен!')
+                continue
+            else:
+                temp.writelines(name)
+
+    os.remove('telephonebook/telephonebook.txt')
+    os.rename("telephonebook/temp.txt", "telephonebook/telephonebook.txt")
+
+
 def main():
-    print(path_file)
-    change_file()
+
+    while input('Вы хотите поработать в телефонной книге (да/нет): ') == 'да'.casefold():
+        print('1 - Посмотреть весь справочник \
+              \n2 - Добавить новый контакт \
+              \n3 - Изменить контакт в телефонной книге \
+              \n4 - Удалить контакт ')
+
+        chapter = input('Выберете цифру соответствующего раздела: ')
+        if chapter == '1':
+            read_file()
+        elif chapter == '2':
+            write_file()
+        elif chapter == '3':
+            change_file()
+        elif chapter == '4':
+            delete_contact()
+        else:
+            print('Такого раздела нет.')
+
 
 path_file = r'telephonebook/telephonebook.txt'
 
